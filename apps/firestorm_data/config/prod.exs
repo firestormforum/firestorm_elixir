46ab1ee72ce0dcdb3fe7f4adcad92830979c4ebe
Josh Adams <josh.rubyist@gmail.com>
2019-10-07 13:02:15 -0500

    Configure prod by database_url env var

--- apps/firestorm_data/config/prod.exs
+++ apps/firestorm_data/config/prod.exs
@@ -1,8 +1,4 @@
 use Mix.Config
 
 config :firestorm_data, FirestormData.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
-  ssl: true,
-  pool_size: 2
+  adapter: Ecto.Adapters.Postgres
--- apps/firestorm_data/lib/firestorm_data/repo.ex
+++ apps/firestorm_data/lib/firestorm_data/repo.ex
@@ -1,4 +1,9 @@
 defmodule FirestormData.Repo do
   use Ecto.Repo, otp_app: :firestorm_data, adapter: Ecto.Adapters.Postgres
   use Scrivener, page_size: 20
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
+
 end
