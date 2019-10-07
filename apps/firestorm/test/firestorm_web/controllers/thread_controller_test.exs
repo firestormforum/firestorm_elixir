861b6f2c3a0e68c6082642b1b91fb8a00a144bcf
Franzé Jr <franzejr@gmail.com>
2019-10-07 11:01:49 -0300

    Update tests for thread controller

--- apps/firestorm/test/firestorm_web/controllers/thread_controller_test.exs
+++ apps/firestorm/test/firestorm_web/controllers/thread_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule FirestormWeb.ThreadControllerTest do
   use FirestormWeb.ConnCase
+  import Firestorm.Factory
 
   alias Firestorm.FirestormAdmin
   alias FirestormWeb.Router.Helpers, as: Routes
@@ -8,6 +9,7 @@ defmodule FirestormWeb.ThreadControllerTest do
     inserted_at: ~N[2010-04-17 14:00:00],
     slug: "some slug",
     title: "some title",
+    category_id: "",
     updated_at: ~N[2010-04-17 14:00:00]
   }
   @update_attrs %{
@@ -18,83 +20,87 @@ defmodule FirestormWeb.ThreadControllerTest do
   }
   @invalid_attrs %{inserted_at: nil, slug: nil, title: nil, updated_at: nil}
 
-  # def fixture(:thread) do
-  #   {:ok, thread} = FirestormAdmin.create_thread(@create_attrs)
-  #   thread
-  # end
+  describe "index" do
+    test "lists all threads", %{conn: conn} do
+      conn = get(conn, Routes.thread_path(conn, :index))
+      assert html_response(conn, 200) =~ "Threads"
+    end
+  end
 
-  # describe "index" do
-  #   test "lists all threads", %{conn: conn} do
-  #     conn = get(conn, Routes.thread_path(conn, :index))
-  #     assert html_response(conn, 200) =~ "Threads"
-  #   end
-  # end
+  describe "new thread" do
+    test "renders form", %{conn: conn} do
+      conn = get(conn, Routes.thread_path(conn, :new))
+      assert html_response(conn, 200) =~ "New Thread"
+    end
+  end
 
-  # describe "new thread" do
-  #   test "renders form", %{conn: conn} do
-  #     conn = get(conn, Routes.thread_path(conn, :new))
-  #     assert html_response(conn, 200) =~ "New Thread"
-  #   end
-  # end
+  describe "create thread" do
+    test "redirects to show when data is valid", %{conn: conn} do
+      category = insert(:category)
 
-  # describe "create thread" do
-  #   test "redirects to show when data is valid", %{conn: conn} do
-  #     conn = post conn, Routes.thread_path(conn, :create), thread: @create_attrs
+      attrs = %{@create_attrs | category_id: category.id}
 
-  #     assert %{id: id} = redirected_params(conn)
-  #     assert redirected_to(conn) == Routes.thread_path(conn, :show, id)
+      conn = post conn, Routes.thread_path(conn, :create), thread: attrs
 
-  #     conn = get(conn, Routes.thread_path(conn, :show, id))
-  #     assert html_response(conn, 200) =~ "Thread Details"
-  #   end
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == Routes.thread_path(conn, :show, id)
 
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post conn, Routes.thread_path(conn, :create), thread: @invalid_attrs
-  #     assert html_response(conn, 200) =~ "New Thread"
-  #   end
-  # end
+      conn = get(conn, Routes.thread_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Thread Details"
+    end
 
-  # describe "edit thread" do
-  #   setup [:create_thread]
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post conn, Routes.thread_path(conn, :create), thread: @invalid_attrs
+      assert html_response(conn, 200) =~ "New Thread"
+    end
+  end
 
-  #   test "renders form for editing chosen thread", %{conn: conn, thread: thread} do
-  #     conn = get(conn, Routes.thread_path(conn, :edit, thread))
-  #     assert html_response(conn, 200) =~ "Edit Thread"
-  #   end
-  # end
+  describe "edit thread" do
+    setup [:create_thread]
 
-  # describe "update thread" do
-  #   setup [:create_thread]
+    test "renders form for editing chosen thread", %{conn: conn, thread: thread} do
+      conn = get(conn, Routes.thread_path(conn, :edit, thread))
+      assert html_response(conn, 200) =~ "Edit Thread"
+    end
+  end
 
-  #   test "redirects when data is valid", %{conn: conn, thread: thread} do
-  #     conn = put conn, Routes.thread_path(conn, :update, thread), thread: @update_attrs
-  #     assert redirected_to(conn) == Routes.thread_path(conn, :show, thread)
+  describe "update thread" do
+    setup [:create_thread]
 
-  #     conn = get(conn, Routes.thread_path(conn, :show, thread))
-  #     assert html_response(conn, 200) =~ "some updated slug"
-  #   end
+    test "redirects when data is valid", %{conn: conn, thread: thread} do
+      category = insert(:category)
 
-  #   test "renders errors when data is invalid", %{conn: conn, thread: thread} do
-  #     conn = put conn, Routes.thread_path(conn, :update, thread), thread: @invalid_attrs
-  #     assert html_response(conn, 200) =~ "Edit Thread"
-  #   end
-  # end
+      attrs = %{@create_attrs | category_id: category.id}
+      attrs = %{attrs | title: "some updated title"}
 
-  # describe "delete thread" do
-  #   setup [:create_thread]
+      conn = put conn, Routes.thread_path(conn, :update, thread), thread: attrs
+      assert redirected_to(conn) == Routes.thread_path(conn, :show, thread)
 
-  #   test "deletes chosen thread", %{conn: conn, thread: thread} do
-  #     conn = delete(conn, Routes.thread_path(conn, :delete, thread))
-  #     assert redirected_to(conn) == Routes.thread_path(conn, :index)
+      conn = get(conn, Routes.thread_path(conn, :show, thread))
+      assert html_response(conn, 200) =~ "some-updated-title"
+    end
 
-  #     assert_error_sent 404, fn ->
-  #       get(conn, Routes.thread_path(conn, :show, thread))
-  #     end
-  #   end
-  # end
+    test "renders errors when data is invalid", %{conn: conn, thread: thread} do
+      conn = put conn, Routes.thread_path(conn, :update, thread), thread: @invalid_attrs
+      assert html_response(conn, 200) =~ "Edit Thread"
+    end
+  end
 
-  # defp create_thread(_) do
-  #   thread = fixture(:thread)
-  #   {:ok, thread: thread}
-  # end
+  describe "delete thread" do
+    setup [:create_thread]
+
+    test "deletes chosen thread", %{conn: conn, thread: thread} do
+      conn = delete(conn, Routes.thread_path(conn, :delete, thread))
+      assert redirected_to(conn) == Routes.thread_path(conn, :index)
+
+      assert_error_sent 404, fn ->
+        get(conn, Routes.thread_path(conn, :show, thread))
+      end
+    end
+  end
+
+  defp create_thread(_) do
+    thread = insert(:thread)
+    {:ok, thread: thread}
+  end
 end
