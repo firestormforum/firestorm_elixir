0793097ae1fa3cbf188e5de0e87998c278d98d13
Josh Adams <josh.rubyist@gmail.com>
2019-10-21 20:57:03 -0500

    Don't check origin in production
    This was causing some issues with websockets...we should actually check
    the origin but until we have a static URL or env vars to set that up at
    build time, this will have to do...

--- apps/firestorm/config/prod.exs
+++ apps/firestorm/config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :firestorm, FirestormWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
+  check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
